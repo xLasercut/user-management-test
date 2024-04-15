9d9488a5ac492056128057cecdf07329546c3d98
Tim Wang <tianxun.wang1@nhs.net>
2024-04-15 10:24:20 +0100

    added ods code check to add roles page

--- src/pages/UserManagementPortal/AddRoles/AddRole.tsx
+++ src/pages/UserManagementPortal/AddRoles/AddRole.tsx
@@ -1,4 +1,4 @@
-import {Button, Checkboxes, Fieldset} from 'nhsuk-react-components';
+import {Button, Checkboxes, Fieldset, Tag} from 'nhsuk-react-components';
 import {useLocation, useNavigate} from 'react-router-dom';
 import {BackLink} from '../../../components/BackLink.tsx';
 import {FormInput} from '../../../components/form/FormInput.tsx';
@@ -7,8 +7,10 @@ import {useForm} from 'react-hook-form';
 import {zodResolver} from '@hookform/resolvers/zod';
 import {FormRoleSelectItem} from '../../../components/form/FormRoleSelectItem.tsx';
 import {COLLECTIONS, ROLES} from '../../../common/constants.ts';
-import {formErrorMessage} from '../../../components/form/helpers.ts';
 import {editUserStore} from '../../../store/edit-user.ts';
+import {getOrgName} from '../../../store/helpers.ts';
+import {useState} from 'react';
+import {formErrorMessage} from '../../../components/form/helpers.ts';
 
 function isNoRoleSelected(data: {[p: string]: any}): boolean {
   for (const collection of Object.values(COLLECTIONS)) {
@@ -23,9 +25,10 @@ function AddRole() {
   const location = useLocation();
   const navigate = useNavigate();
   const addRoleToAdd = editUserStore(state => state.addRoleToAdd);
+  const [orgName, setOrgName] = useState<string>('Not Checked');
 
   const formSchemaRaw: ZodRawShape = {
-    odsCode: z.string().trim().min(1),
+    odsCode: z.string().trim().min(1).toUpperCase(),
   };
 
   for (const collection of Object.values(COLLECTIONS)) {
@@ -33,13 +36,15 @@ function AddRole() {
   }
 
   const formSchema = z.object(formSchemaRaw);
-  type TFormSchema = z.infer<typeof formSchema>
+  type TFormSchema = z.infer<typeof formSchema>;
 
   const {
     control,
     handleSubmit,
     formState: {errors},
     setError,
+    getValues,
+    clearErrors,
   } = useForm<TFormSchema>({
     resolver: zodResolver(formSchema),
     values: {
@@ -47,9 +52,23 @@ function AddRole() {
     },
   });
 
+  function validOdsCode(): boolean {
+    const organisationName = getOrgName(getValues('odsCode').toUpperCase());
+    if (!organisationName) {
+      setError('odsCode', {message: 'Invalid ODS code'});
+      return false;
+    }
+    setOrgName(organisationName);
+    clearErrors('odsCode');
+    return true;
+  }
+
   const onSubmitForm = handleSubmit(data => {
     if (isNoRoleSelected(data)) {
       setError('roles', {message: 'You must select at least one role'});
+      return;
+    }
+    if (!validOdsCode()) {
       return;
     }
 
@@ -73,16 +92,33 @@ function AddRole() {
       <div className='nhsuk-u-width-two-thirds'>
         <BackLink to={location.state.from}>Cancel</BackLink>
         <form onSubmit={onSubmitForm}>
-          <FormInput<TFormSchema>
-            control={control}
-            formField={'odsCode'}
-            errors={errors}
-            label={'ODS Code'}
-          ></FormInput>
+          <div className='nhsuk-grid-row'>
+            <div className='nhsuk-grid-column-two-thirds'>
+              <FormInput<TFormSchema>
+                control={control}
+                formField={'odsCode'}
+                errors={errors}
+                label={'ODS Code'}
+              ></FormInput>
+            </div>
+            <div className='nhsuk-grid-column-one-third'>
+              <Button
+                type={'button'}
+                onClick={validOdsCode}
+                className='nhsuk-u-margin-3 nhsuk-u-padding-2 nhsuk-u-padding-right-3 nhsuk-u-padding-left-3'
+              >
+                Check
+              </Button>
+            </div>
+          </div>
           <Fieldset>
-            <Fieldset.Legend>
-              <b>Select collection and role</b>
-            </Fieldset.Legend>
+            <Fieldset.Legend>Organisation Name</Fieldset.Legend>
+            <p>
+              <Tag>{orgName}</Tag>
+            </p>
+          </Fieldset>
+          <Fieldset>
+            <Fieldset.Legend>Select collection and role</Fieldset.Legend>
             <Checkboxes error={formErrorMessage<TFormSchema>('roles', errors)}>
               {Object.values(COLLECTIONS).map(collection => (
                 <FormRoleSelectItem<TFormSchema>
--- src/store/helpers.ts
+++ src/store/helpers.ts
@@ -3,6 +3,7 @@ import {TUserRole} from '../models/user-management-api/common.ts';
 const ORG_CODES: Record<string, string> = {
   X26: 'NHS England',
   X27: 'NHS Digital',
+  RWD: 'UNITED LINCOLNSHIRE HOSPITALS NHS TRUST'
 };
 
 function getOrgName(odsCode?: string | null): string {
